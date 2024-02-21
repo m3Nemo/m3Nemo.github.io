9a757e8d51b67fd0de60a7ed508c4f1d4bf0fc94
m3Nemo <112379637+m3Nemo@users.noreply.github.com>
2024-02-21 14:09:49 +0100

    Various Design Changes

--- script.js
+++ script.js
@@ -18,3 +18,48 @@ function toggleSection(goTo) {
     navigateTo.classList.remove("hidden");
     navigateTo.classList.add("active");
 }
+
+function typingAnimation(className ,text, speed, delay) {
+
+    var options = {
+        strings: [text],
+        typeSpeed: speed || 50,
+        showCursor: false,
+        loop: false,
+        startDelay: delay || 0
+    };
+
+    var typed = new Typed(`.${className}`, options);
+}
+document.addEventListener('DOMContentLoaded', function() {
+    emoteAnimation(600);
+    setTimeout(() => {
+        emoteAnimation(1800);
+    }, 1300);
+    typingAnimation("typed-title" ,"Hallo" , 75);
+    typingAnimation("typed-subtitle" ,"Ich bin Daniel.", 75, 1300);
+});
+
+function emoteAnimation(duration) {
+    var emote = document.querySelector('.emote');
+    var emoteList = ["^_^", "^̮ ^", "^̥ ^"];
+    var index = 0;
+    var clear = false;
+
+    setTimeout(() => {
+        clear = true;
+        setTimeout(() => {
+            emote.textContent = "°˽°";
+        }, 200);
+    }, duration);
+
+    function updateEmote() {
+        emote.textContent = emoteList[index];
+        index = (index + 1) % emoteList.length;
+        if(clear === false) {
+            setTimeout(updateEmote, 75);
+        }
+    }
+
+    updateEmote(); // Start the animation
+}
